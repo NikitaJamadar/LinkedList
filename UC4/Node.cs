f580c1a6a7d6000b1cdc29800cc9be6f12d40186
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 21:05:20 +0530

    inserting elements in linked list

---
+++ UC4/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+    }
+}
---
+++ UC4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            //List.Add(30);
+            List.Add(70);
+          
+            List.insert(1, 30);
+            List.Display();
+
+        }
+    }
+}
