0fee964308e8e4e34e8ed7897dbca42c1a9f1fd0
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:34:33 +0530

    creating linked list

---
+++ UC1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
+        }
+    }
+}
