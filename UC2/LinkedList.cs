3aa9593cf445cc3d8994c7eeeea150bf373fc6e1
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 21:02:43 +0530

    Adding elements in linked list

---
+++ UC2/LinkedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void AddLast(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        internal void AddFirst(int data)
+        {
+            Node NewNode = new Node(data);
+            if (head == null)
+            {
+                Console.WriteLine("linked list is null");
+                return;
+            }
+            else
+            {
+                NewNode.next = head;
+                head = NewNode;
+            }
+           
+        }
+    }
+}
---
+++ UC2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.AddLast(70);
+            List.AddFirst(30);
+            List.AddFirst(56);
+            List.Display();
+        }
+    }
+}
