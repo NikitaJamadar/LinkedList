b9e79c847c501e2835ca6134236d5010099aad9b
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 22:01:37 +0530

    Delete last element in linked list

---
+++ UC6/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC6
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+    }
+}
---
+++ UC6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+
+            Console.WriteLine("\n Create Linked List is \n");
+            List.Display();
+
+            List.deleteNode(2);
+
+            Console.WriteLine("\n Linked List after Deletion: ");
+            List.Display();
+        }
+    }
+}
