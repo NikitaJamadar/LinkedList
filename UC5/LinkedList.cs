71ecded32d2aac36c960028c490752add9a924b4
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 21:07:07 +0530

    Deleting first element in linked list

---
+++ UC5/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC5
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void deleteFirstNode()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            else
+            {
+                head = head.next;
+            }
+        }
+        internal void Display()
+        {
+            Node temp =head;
+           
+            while (temp != null)
+            {
+                Console.Write(temp.data + "-->");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ UC5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+
+            Console.WriteLine("\n Create Linked List is \n");
+            List.Display();
+
+            List.deleteFirstNode();
+
+            Console.WriteLine("\n Linked List after Deletion : ");
+            List.Display();
+        }
+    }
+}
