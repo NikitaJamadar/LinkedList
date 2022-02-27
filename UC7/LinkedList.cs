1b4a509828e248733aea02990d5bc4242970f3b8
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 22:00:17 +0530

    search 30 element in linked list

---
+++ UC7/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC7
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            node.data = data;
+            node.next = null;
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public bool Search(int n)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.data == n)
+                {
+                    Console.WriteLine("\n Element is Present");
+                    Console.WriteLine("\n Display the Current Node : " + current.data);
+                }
+                current = current.next;
+            }
+            //Console.WriteLine("\n Element is not Preset");
+            return false;
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + "-->");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ UC7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(40);
+            List.Add(70);
+
+            Console.Write("\n Create Linked List is : ");
+            List.Display();
+
+            Console.WriteLine("\n \n Searched the Node : ");
+            List.Search(40);
+        }
+    }
+}
