63693719148fb9b9e2a0d41485b3f4f35a49f9c9
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 21:04:04 +0530

    Appending elements in linked list

---
+++ UC3/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Append(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "-->");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ UC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Append(56);
+            List.Append(30);
+            List.Append(70);
+            List.Display();
+        }
+    }
+}
