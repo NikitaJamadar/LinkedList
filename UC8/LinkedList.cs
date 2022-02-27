1b793ba108c359139affb1fbdeb01f0c8aabf6c0
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 22:03:12 +0530

    Insert 40 element in linked list

---
+++ UC8/LinkedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC8
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void insert(int position, int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+
+            if (this.head == null)
+            {
+                if (position != 0)
+                {
+                    return;
+                }
+                else
+                {
+                    this.head = node;
+                }
+            }
+
+            if (head != null && position == 0)
+            {
+                node.next = this.head;
+                this.head = node;
+                return;
+            }
+
+            Node current = this.head;
+            Node previous = null;
+
+            int i = 0;
+
+            while (i < position)
+            {
+                previous = current;
+                current = current.next;
+
+                if (current == null)
+                {
+                    break;
+                }
+                i++;
+            }
+            node.next = current;
+            previous.next = node;
+            Console.WriteLine("\n value inserted in between \n");
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "-->");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ UC8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
+            List.insert(2, 40);
+            List.Display();
+        }
+    }
+}
